bdcaf1a48c1eb1a1919f8024909e45502d444051
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-06-16 20:20:36 -0400

    Begin playDrawPoker function

---
+++ ag_poker.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*
+   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
+   Tutorial 14
+   Tutorial Case
+
+   Author: Ian Jones
+   Date:   6/16/21
+   
+   Filename: ag_poker.js
+
+*/
+
+// Begin playDrawPoker function
+window.addEventListener("load", playDrawPoker);
+
+function playDrawPoker() {
+   var dealButton = document.getElementById("dealB")
+   var drawButton = document.getElementById("drawB")
+   var standButton = document.getElementById("standB")
+   var resetButton = document.getElementById("restB")
+   var handValueText = document.getElementById("handValue")
+   var betSelection = document.getElementById("bet")
+   var bankBox = document.getElementById("bank")
+}
